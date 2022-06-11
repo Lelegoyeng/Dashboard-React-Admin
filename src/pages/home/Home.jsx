d2b628be31a3d0199e1dae5c33bcb53e1b1390c5
kevin rubama putra <lelegoyengs@gmail.com>
2022-06-11 16:23:42 +0700

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widgets/Widget";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+        <Sidebar />
+        <div className="homeContainer">
+          <Navbar />
+          <div className="widgets">
+            <Widget type="user" />
+            <Widget type="order" />
+            <Widget type="earning" />
+            <Widget type="balance" />
+          </div>
+          <div className="charts">
+
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
